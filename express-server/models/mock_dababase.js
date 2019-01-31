666bd3ac256b009a326260a80c1e648b0905c35b
Vadym Ivanchuk <vadym.ivanchuk@gmail.com>
2019-01-31 17:22:13 +0100

    Añadidos icono cierre sesión & estilos de la pág. de inicio

--- express-server/models/mock_dababase.js
+++ express-server/models/mock_dababase.js
@@ -3,8 +3,8 @@ var Entrevista = require('./entrevista');
 var Item = require('./item');
 var Valor = require('./valor');
 
-var usuario_1 = new Usuario(1, 'test1', 'Paciente', 'Prueba', 'Prueba');
-var usuario_2 = new Usuario(2, 'a', 'Admin', 'Prueba', 'Prueba');
+var usuario_1 = new Usuario(1, 'test1', 'Paciente', 'Apellido1', 'Apellido2');
+var usuario_2 = new Usuario(2, 'a', 'Admin', 'Apellido1', 'Apellido2');
 usuarios = [usuario_1, usuario_2];
 
 var clave_1 = 'pass1';
