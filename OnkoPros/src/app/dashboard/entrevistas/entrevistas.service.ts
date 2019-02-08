2059cafa281b99c78f7cdd32b3ec7fc0dd340dbb
Vadym Ivanchuk <vadym.ivanchuk@gmail.com>
2019-02-08 16:39:46 +0100

    Añadido spinner a las pág. de entrevistas & items

--- OnkoPros/src/app/dashboard/entrevistas/entrevistas.service.ts
+++ OnkoPros/src/app/dashboard/entrevistas/entrevistas.service.ts
@@ -7,6 +7,8 @@ import { Entrevista } from './entrevista';
 import { Item } from './item';
 import { Valor } from './valor';
 
+import { NgxSpinnerService } from 'ngx-spinner';
+
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json'
@@ -21,7 +23,8 @@ export class EntrevistasService {
   private _entrevistasURL = 'http://localhost:8080/api/entrevistas';  // URL de la web api
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private spinner: NgxSpinnerService
   ) { }
 
   /**
@@ -44,23 +47,41 @@ export class EntrevistasService {
    * Extrae las entrevistas del servidor
    */
   getEntrevistas(): Observable<Entrevista[]> {
-    return this.http.get<Entrevista[]>(this.entrevistasURL);
+    this.spinner.show();
+    return this.http.get<Entrevista[]>(this.entrevistasURL)
+      .pipe(
+        finalize(() => {
+          this.spinner.hide();
+        })
+      );
   }
 
   /**
    * Extrae la siguiente pregunta
    */
   getItem(id: number): Observable<Item> {
+    this.spinner.show();
     const url = `${this.entrevistasURL}/${id}`;
-    return this.http.get<Item>(url);
+    return this.http.get<Item>(url)
+      .pipe(
+        finalize(() => {
+          this.spinner.hide();
+        })
+      );
   }
 
   /**
    * Envío de la respuesta del usuario al servidor
    */
   postValor(entrevistaId: number, valor: Valor): Observable<any> {
+    this.spinner.show();
     const url = `${this.entrevistasURL}/${entrevistaId}`;
-    return this.http.post<any>(url, valor, httpOptions);
+    return this.http.post<any>(url, valor, httpOptions)
+      .pipe(
+        finalize(() => {
+          this.spinner.hide();
+        })
+      );
   }
 
 }
