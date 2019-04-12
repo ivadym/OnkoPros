33496f5399d3ad06680b5a044ba05e133792a334
Vadym Ivanchuk <vadym.ivanchuk@gmail.com>
2019-04-12 12:18:45 +0200

    Actualización de la BBDD: Cambio al estado "en progreso" (entrevista)

--- private_server/express-server/models/valorDB.js
+++ private_server/express-server/models/valorDB.js
@@ -10,9 +10,8 @@ const config = require('./config');
 exports.setItemValor = function(idUsuario, item) {
     return new Promise(function(resolve, reject) {
         // TODO:
-        // Guardar valor/es respondido/s
-        // Actualizar estado de la OP_ENTREVISTA: 'en proceso'
-
+        // Guardar valor/es respondido/s (campo texto)
+        
         var connection = new Connection(config.auth);
         var query = `INSERT INTO OP_ENTREVISTA_ITEM (IdEntrevistaItem, IdEntrevistaUsuario, IdItem, Estado)
                     VALUES ((SELECT ISNULL(MAX(IdEntrevistaItem), 0)+1 FROM OP_ENTREVISTA_ITEM), (SELECT IdEntrevistaUsuario FROM OP_ENTREVISTA WHERE IdUsuario=@idUsuario AND IdEntrevista=@idEntrevista), @idItem, 1);`;
@@ -32,6 +31,46 @@ exports.setItemValor = function(idUsuario, item) {
                 request.addParameter('idEntrevista', TYPES.Int, item.IdEntrevista);
                 request.addParameter('idItem', TYPES.Int, item.IdItem);
 
+                request.on('requestCompleted', function () {
+                    actualizarEstadoEntrevista(idUsuario, item)
+                    .then(function(res) {
+                        resolve(res);
+                    })
+                    .catch(function(error) {
+                        resolve(error);
+                    });
+                });
+
+                connection.execSql(request);
+            }
+        });
+    });
+}
+
+/**
+ * Actualiza el estado de la entrevista (en progreso)
+ */
+function actualizarEstadoEntrevista(idUsuario, item) {
+    return new Promise(function(resolve, reject) {
+        var connection = new Connection(config.auth);
+        var query = `UPDATE OP_ENTREVISTA
+                    SET Estado=10
+                    WHERE IdUsuario=@idUsuario AND IdEntrevista=@idEntrevista;`;
+
+        connection.on('connect', function(err) {
+            if (err) {
+                reject(err);
+            } else {
+                request = new Request(query, function(err, rowCount, rows) {
+                    if (err) {
+                        reject(err);
+                    }
+                    connection.close();
+                });
+
+                request.addParameter('idUsuario', TYPES.Int, idUsuario);
+                request.addParameter('idEntrevista', TYPES.Int, item.IdEntrevista);
+                
                 request.on('requestCompleted', function () {
                     resolve(item);
                 });
